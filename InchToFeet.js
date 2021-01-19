d1dbec735a5217f8e3fa563e7a4d234f1c9c7589
saifur116 <67361625+saifur116@users.noreply.github.com>
2021-01-19 13:17:04 +0600

    first commit

---
+++ InchToFeet.js
@@ -0,0 +1,24 @@
+var inch=12;
+var result=inch/12;
+console.log(result  +   "feet")
+var feet=12;
+var result2=feet*12;
+console.log(result2+"inch")
+
+//use function
+
+
+function inch3(num){
+
+var inch2=num;
+var result3=inch2/12;
+return result3;
+
+
+
+}
+var nani=inch3(12);
+var nana=inch3(24);
+
+console.log(nani+"nani feet")
+console.log(nana+"nana feet")
